ca81b7a91d7222deb255f555647c398127394e59
Codei1991 <rzslmn90@gmail.com>
2024-02-25 14:52:44 +0330

    add setUp Nav Graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -66,4 +68,47 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    //room db
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    /*implementation("androidx.room:room-paging:2.6.1")*/
+
+    //data store
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    // dagger hilt with ksp
+    implementation("com.google.dagger:hilt-android:2.48")
+    /*ksp("com.google.dagger:hilt-android-compiler:2.48")
+    ksp ("androidx.hilt:hilt-compiler:1.2.0")*/
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+    ksp ("com.google.dagger:dagger-compiler:2.48") // Dagger compiler
+    ksp ("com.google.dagger:hilt-compiler:2.48")   // Hilt compiler
+
+    //compose navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    //Gson
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    //lottie animation
+    implementation ("com.airbnb.android:lottie-compose:6.3.0")
+
+    //coil - load image from url
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    //swipe refresh
+    implementation ("com.google.accompanist:accompanist-swiperefresh:0.27.0")
+
+    //system ui controller
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+
+
 }
--- app/src/main/java/com/example/digikala/MainActivity.kt
+++ app/src/main/java/com/example/digikala/MainActivity.kt
@@ -1,46 +1,46 @@
 package com.example.digikala
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.digikala.navigation.SetUpNavGraph
 import com.example.digikala.ui.theme.DigikalaTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DigikalaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+                        // TODO
+                    }
                 ) {
-                    Greeting("Android")
+                    SetUpNavGraph(navController)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DigikalaTheme {
-        Greeting("Android")
+        //
     }
 }
