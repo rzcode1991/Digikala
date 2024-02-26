96416cf358aac204a56c6ba1b7482fba940a9e8e
Codei1991 <rzslmn90@gmail.com>
2024-02-26 12:20:43 +0330

    designing splash screen

--- app/src/main/java/com/example/digikala/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/digikala/ui/screens/SplashScreen.kt
@@ -1,12 +1,68 @@
 package com.example.digikala.ui.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.digikala.R
+import com.example.digikala.navigation.Screen
+import com.example.digikala.ui.components.Loading3Dots
+import com.example.digikala.ui.theme.splashBg
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
     navController: NavHostController
 ){
-    Text(text = "SplashScreen")
+    Splash()
+    LaunchedEffect(true){
+        delay(2500)
+        navController.navigate(Screen.Home.route)
+    }
+}
+
+@Composable
+fun Splash(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(splashBg),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.digi_logo),
+            contentDescription = "",
+            modifier = Modifier.size(250.dp)
+            )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(100.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.digi_txt_white),
+                contentDescription = "",
+                modifier = Modifier.height(30.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Loading3Dots(false)
+        }
+    }
 }
